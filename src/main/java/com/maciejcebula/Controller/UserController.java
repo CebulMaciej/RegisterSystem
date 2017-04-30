ed8120ff82f01aaf112aba16523e0097b162443e
CebulMaciej <maciejcebula96@gmail.com>
2017-05-01 01:09:29 +0200

    Logging out - finally exist

--- src/main/java/com/maciejcebula/Controller/UserController.java
+++ src/main/java/com/maciejcebula/Controller/UserController.java
@@ -6,7 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Maciej Cebula on 25.04.2017.
@@ -43,6 +49,16 @@ public class UserController {
         {
             return "redirect:/homebad";
         }
+    }
+    @GetMapping(value = "/logout")
+    public String page4(@ModelAttribute User user, WebRequest request, SessionStatus status) {
+        /**
+         * store User ...
+         */
+        status.setComplete();
+        request.removeAttribute("user", WebRequest.SCOPE_SESSION);
+        request.removeAttribute("name", WebRequest.SCOPE_SESSION);
+        return "redirect:/";
     }
 
 }
